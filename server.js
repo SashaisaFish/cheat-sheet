2ff25bcc91a19dadb2950aea34c36d725b21acfc
SashaisaFish <98102700+SashaisaFish@users.noreply.github.com>
2022-11-14 17:58:51 +0000

    questionable structure

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
 const port = 5000;
 
 // set up static webpages
-app.use("/index", express.static("1-index-page"));
-app.use("/html", express.static("2-html-page"));
-app.use("/css", express.static("3-css-page"));
-app.use("/js", express.static("4-js-page"));
-app.use("/dom", express.static("5-dom-page"));
-app.use("/other", express.static("6-other-page"));
+app.use("/index/", express.static("1-index-page"));
+app.use("/html/", express.static("2-html-page"));
+app.use("/css/", express.static("3-css-page"));
+app.use("/js/", express.static("4-js-page"));
+app.use("/dom/", express.static("5-dom-page"));
+app.use("/other/", express.static("6-other-page"));
 
 // listen
 app.listen(port, () => {
